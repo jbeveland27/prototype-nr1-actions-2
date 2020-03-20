9f956d3c6bad4bda412f2fe371565f7d994d68b4
Justin Eveland <jeveland@newrelic.com>
2020-03-20 11:43:19 -0400

    init repo

---
+++ nerdlets/prototype-nr1-actions-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class PrototypeNr1ActionsNerdletNerdlet extends React.Component {
+  render() {
+    return <h1>Hello, prototype-nr1-actions-nerdlet Nerdlet v5!</h1>;
+  }
+}
